ba51353bea298b09debacd4c54997b43252dbefa
Lucas Burigo <burigolucas@gmail.com>
2025-03-20 19:39:50 +0100

    Init commit

---
+++ Containerfile
@@ -0,0 +1,62 @@
+FROM registry.access.redhat.com/ubi9/openjdk-21:latest AS builder
+
+ARG KCB_RELEASE="26.1-SNAPSHOT"
+ARG KC_RELEASE="26.1.4"
+ARG KCB_RESULTS_PATH="/opt/keycloak-benchmark/results"
+
+ENV NAME=keycloak-benchmark
+
+ENV SUMMARY="Platform for running Keycloak performance tests and serving results over Apache httpd server" \
+    DESCRIPTION="Apache httpd is started in the backgroup and Keycloak benchmark tests run on foreground. \
+Parameters for the performance tests can be set using environment variables. \
+The HTML report of results is served by Apache httpd server."
+
+LABEL summary="$SUMMARY" \
+      description="$DESCRIPTION" \
+      io.k8s.description="$DESCRIPTION" \
+      io.k8s.display-name="Keycloak-benchmark" \
+      io.openshift.expose-services="8080:http" \
+      io.openshift.tags="builder,$NAME" \
+      name="ubi9/$NAME" \
+      maintainer="Lucas Burigo" \
+      version="1"
+
+EXPOSE 8080
+
+USER root
+RUN microdnf install jq util-linux httpd -y && \
+    microdnf clean all && \
+    rm -rf /var/cache/yum
+
+WORKDIR /opt
+ADD https://github.com/keycloak/keycloak/releases/download/${KC_RELEASE}/keycloak-${KC_RELEASE}.tar.gz .
+ADD https://github.com/keycloak/keycloak-benchmark/releases/download/${KCB_RELEASE}/keycloak-benchmark-${KCB_RELEASE}.tar.gz .
+RUN tar -zxf keycloak-${KC_RELEASE}.tar.gz && \
+    tar -zxf keycloak-benchmark-${KCB_RELEASE}.tar.gz && \
+    mv keycloak-${KC_RELEASE} keycloak && \
+    mv keycloak-benchmark-${KCB_RELEASE} keycloak-benchmark && \
+    rm /opt/keycloak-${KC_RELEASE}.tar.gz && \
+    rm /opt/keycloak-benchmark-${KCB_RELEASE}.tar.gz && \
+    mkdir ${KCB_RESULTS_PATH} && \
+    chgrp -R 0 /opt && \
+    chmod -R g=u /opt
+
+RUN sed -i 's/#ServerName.*/ServerName localhost:8080/' /etc/httpd/conf/httpd.conf && \
+    sed -i 's/Listen 80/Listen 8080/' /etc/httpd/conf/httpd.conf && \
+    sed -i 's/User apache/User default/' /etc/httpd/conf/httpd.conf && \
+    sed -i 's/Group apache/Group root/' /etc/httpd/conf/httpd.conf && \
+    chown -R default:0 /var/log/httpd && \
+    chmod -R g=u /var/log/httpd && \
+    chown -R default:0 /run/httpd && \
+    chmod -R g=u /run/httpd && \
+    chown -R default:0 /etc/httpd && \
+    chmod -R g=u /etc/httpd && \
+    rm -rf /var/www/html && \
+    ln -s ${KCB_RESULTS_PATH} /var/www/html && \
+    chown -R default:0 /var/www/html && \
+    chmod -R g=u /var/www/html
+
+USER default
+COPY start_perf_test.sh .
+
+CMD ["bash", "start_perf_test.sh"]
---
+++ start_perf_test.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+
+_=${KCB_USERS_PER_SEC:=1}
+_=${KCB_USERS_PER_REALM:=1}
+_=${KCB_MEASUREMENT:=30}
+_=${KCB_SCENARIO:="keycloak.scenario.authentication.AuthorizationCode"}
+_=${KCB_USER_THINK_TIME:="0"}
+_=${KCB_REALM_NAME:="realm-0"}
+_=${KCB_USE_INCREMENT:="false"}
+_=${KCB_INCREMENT:="32"}
+_=${KCB_RAMP_UP:="5"}
+_=${KCB_WARM_UP:="0"}
+_=${KCB_SLA_ERROR_PERCENTAGE:="0"}
+_=${KCB_SLA_MEAN_RESPONSE_TIME:="300"}
+_=${KCB_USE_ALL_LOCAL_ADDRESSES:="false"}
+_=${KCB_FILTER_RESULTS:="false"}
+
+prefix=KCB_
+typeset -p | awk '$3 ~ "^"pfx { print $3 }' pfx="$prefix" 
+
+if [[ -z "${KC_SERVER_URL}" ]]; then
+  echo "Environment variable 'KC_SERVER_URL' is not set"
+  exit 1
+fi
+
+if [[ -z "${KC_ADMIN_USERNAME}" ]]; then
+  echo "Environment variable 'KC_ADMIN_USERNAME' is not set"
+  exit 1
+fi
+
+if [[ -z "${KC_ADMIN_PASSWORD}" ]]; then
+  echo "Environment variable 'KC_ADMIN_PASSWORD' is not set"
+  exit 1
+fi
+
+PATH=$PATH:/opt/keycloak/bin:/opt/keycloak-benchmark/bin
+
+# Start apache server and update page periodically with links for results
+cd /opt/keycloak-benchmark/results
+while ((1))
+do
+  echo '<h1>Keycloak Performance Tests</h1>' > index.html.tmp; \
+  DATETIME=$(date); \
+  echo "<h2>Last updated on: $DATETIME</h2>" >> index.html.tmp; \
+  find . \
+    -mindepth 2 \
+    -name "index.html" \
+    -exec printf "<hr><a href = \"%s\">###%s###</a>\n</hr>" {} {} \; \
+    >> index.html.tmp; \
+  sed -i 's@###\./@@g' index.html.tmp; \
+  sed -i 's@/index\.html###@@g' index.html.tmp; \
+  mv index.html.tmp index.html; \
+  sleep 1; \
+done &
+/usr/sbin/httpd
+
+# Login to Keycloak
+kcadm.sh config credentials \
+  --server ${KC_SERVER_URL} \
+  --realm master \
+  --user "${KC_ADMIN_USERNAME}" \
+  --password "${KC_ADMIN_PASSWORD}"
+
+# Start performance tests
+if [ "$KCB_USE_INCREMENT" = true ]; then
+  KCB_INCREMENT="--increment=$KCB_INCREMENT"
+else
+  KCB_INCREMENT=""
+fi
+
+kcb.sh \
+    --server-url="${KC_SERVER_URL}" \
+    --realm-name="${KCB_REALM_NAME}" \
+    --users-per-realm=$KCB_USERS_PER_REALM \
+    --measurement=$KCB_MEASUREMENT \
+    --users-per-sec=$KCB_USERS_PER_SEC \
+    --scenario="${KCB_SCENARIO}" \
+    --user-think-time=$KCB_USER_THINK_TIME \
+    --sla-error-percentage=$KCB_SLA_ERROR_PERCENTAGE \
+    --sla-mean-response-time=${KCB_SLA_MEAN_RESPONSE_TIME} \
+    $KCB_INCREMENT \
+    --ramp-up=$KCB_RAMP_UP \
+    --warm-up=$KCB_WARM_UP \
+    --use-all-local-addresses=${KCB_USE_ALL_LOCAL_ADDRESSES} \
+    --filter-results=${KCB_FILTER_RESULTS}
+
+# Wait for SIGTERM
+trap 'trap - TERM; kill -s TERM -- -$$' TERM
+tail -f /dev/null & wait
+exit 0
